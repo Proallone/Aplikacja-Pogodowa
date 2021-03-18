cb9ab00c3b40e8c01ba2de64426a8a136ed52f5d
kamil.sz1999 <kamil.sz1999@gmail.com>
2021-03-18 12:41:42 +0100

    Yes

--- app/src/main/java/com/example/pwiweatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/pwiweatherapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 /** POLSKA GUROM **/
+/**omamale**/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
